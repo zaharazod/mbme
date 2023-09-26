960a997799c97ac4fb66031d64006abb71b8ddb8
Matt Barry <matt@hazelmollusk.org>
2023-09-26 07:30:07 -0400

    commented out apache utility function

--- mbme/wsgi.py
+++ mbme/wsgi.py
@@ -14,3 +14,19 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mbme.settings')
 
 application = get_wsgi_application()
+
+# Determines if WSGI is running in embedded/daemon mode
+# def application(environ, start_response):
+#     status = '200 OK'
+
+#     if not environ['mod_wsgi.process_group']:
+#         output = u'EMBEDDED MODE'
+#     else:
+#         output = u'DAEMON MODE'
+
+#     response_headers = [('Content-Type', 'text/plain'),
+#                         ('Content-Length', str(len(output)))]
+
+#     start_response(status, response_headers)
+
+#     return [output.encode('UTF-8')]
