62957aeb8215f2c86ebb1d1a7600d4c505bf83bd
Matt Barry <matt@hazelmollusk.org>
2023-09-26 15:40:03 -0400

    add root redirect

--- mbme/urls.py
+++ mbme/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('words/', include('apps.blog.blog_v1.urls', namespace='blog_v1'))
+    path('words/', include('apps.blog.blog_v1.urls', namespace='blog_v1')),
+    path('', lambda req: redirect('blog_v1:index'))
 ]
