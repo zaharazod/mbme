424bdf18b3351d06997bc0bb87d4591e2d583b9e
Matt Barry <matt@hazelmollusk.org>
2023-09-27 09:36:26 -0400

    mbme_extras, add settings (use local_settings.py)

---
+++ mbme/settings.py
@@ -0,0 +1,80 @@
+'''
+mbme project settings
+
+    see also
+https://docs.djangoproject.com/en/dev/topics/settings/
+https://docs.djangoproject.com/en/dev/ref/settings/
+
+'''
+
+from pathlib import Path
+from .local_settings import *  # noqa
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_quill',
+    'auditlog',
+    'apps.mbme_extras',
+    'apps.blog.blog_v1',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'auditlog.middleware.AuditlogMiddleware'
+]
+
+ROOT_URLCONF = 'mbme.urls'
+
+TEMPLATE_DIR = BASE_DIR / 'templates'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATE_DIR.as_posix()],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'mbme.wsgi.application'
+
+PASSWORD_VALIDATION = 'django.contrib.auth.password_validation'
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': f'{PASSWORD_VALIDATION}.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': f'{PASSWORD_VALIDATION}.MinimumLengthValidator',
+    },
+    {
+        'NAME': f'{PASSWORD_VALIDATION}.CommonPasswordValidator',
+    },
+    {
+        'NAME': f'{PASSWORD_VALIDATION}.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
