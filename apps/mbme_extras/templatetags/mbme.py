a2170ea5e498d0c612b692c853860647a312f808
Matt Barry <matt@hazelmollusk.org>
2023-09-29 18:32:07 -0400

    fix image_click w/multiple images

--- apps/mbme_extras/templatetags/mbme.py
+++ apps/mbme_extras/templatetags/mbme.py
@@ -17,6 +17,7 @@ def random_fortune(*args, **kwargs):
 @register.filter(is_safe=True)
 @stringfilter
 def image_click(html, func):
-    return re.sub(r'<img.*src="data:image\/([a-zA-Z]*);base64,([^"]*)"s>',
-                  f'<img src="data:image/\1;base64,\2" onclick="{func}" />',
+    # return re.sub(r'<img.*src="data:image\/([a-zA-Z]*);base64,([^"]*)"s>',
+    return re.sub(r'<img ([^>]*)>',
+                  f'<img onclick="{func}" \\1>',
                   html)
