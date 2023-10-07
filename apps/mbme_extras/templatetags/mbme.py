b2a37d24f95d82f10b0339bd082cfb753f2d9626
Matt Barry <matt@hazelmollusk.org>
2023-10-07 16:15:58 -0400

    font_tag

--- apps/mbme_extras/templatetags/mbme.py
+++ apps/mbme_extras/templatetags/mbme.py
@@ -2,6 +2,7 @@ import re
 from django.template import Library
 from django.template.defaultfilters import stringfilter
 # from django.conf import settings
+from django.utils.safestring import mark_safe
 
 from vaticinator import Vaticinator
 
@@ -24,3 +25,32 @@ def image_click(html, func):
     return re.sub(r'<img ([^>]*)>',
                   f'<img onclick="{func}" \\1>',
                   html)
+
+
+@register.simple_tag
+def font_links():
+    FONTS = (
+        'Shadows+Into+Light',
+        'IM+Fell+English+SC',
+        'Sedgwick+Ave+Display',
+        'Acme',
+        'Nothing+You+Could+Do',
+        'VT323',
+        'Black+Ops+One',
+        'Dosis:wght@200;300;400;500;600;700')
+    links = [
+        '<link rel="preconnect" \
+            href="https://fonts.googleapis.com" />',
+        '<link rel="preconnect" \
+            href="https://fonts.gstatic.com" crossorigin />'
+    ]
+    # links.append(
+    #     '<link href="https://fonts.googleapis.com/css2?'
+    #     + "family&".join([font for font in FONTS])
+    #     + '&display=swap rel="stylesheet" />'
+    # )
+    for font in FONTS:
+        links.append(
+            f'<link href="https://fonts.googleapis.com/css2?family={font}&display=swap" rel="stylesheet" / >'
+        )
+    return mark_safe('\n'.join(links))
