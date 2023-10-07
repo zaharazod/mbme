c59aae1f061253c2297ee11d34482ff002205c7e
Matt Barry <matt@hazelmollusk.org>
2023-10-07 11:10:56 -0400

    template formatting

--- apps/blog/blog_v1/urls.py
+++ apps/blog/blog_v1/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import post_list, post_highlight  # , post_detail
+from .feeds import PostFeed
 
 app_name = 'blog_v1'
 urlpatterns = [
     path('', post_highlight, name='index'),
     path('tag/<str:tag>/', post_list, name='post-tag-list'),
+    path('rss/', PostFeed()),
     path('<str:slug>/detail/', post_highlight,
          name='post_detail', kwargs={'full': True}),
     path('<str:slug>/', post_highlight, name='post'),
