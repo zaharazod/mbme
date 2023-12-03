57336e4ee12a96eb818e98cf2403a5cd43762c44
Matt Barry <matt@hazelmollusk.org>
2023-12-02 23:59:47 -0500

    add.google.auth

--- mbme/settings.py
+++ mbme/settings.py
@@ -144,6 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.facebook.FacebookOAuth2',
     'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 ]
 LOGIN_URL = 'mbme:login'
