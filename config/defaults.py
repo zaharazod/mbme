47ffa475bb47c778f9e5b61f21869471530ba4a3
Matt Barry <matt@hazelmollusk.org>
2023-12-31 16:24:28 -0500

    add.default.config

--- config/defaults.py
+++ config/defaults.py
@@ -1,1 +1,31 @@
-settings = {}
+settings = {
+    'domains': ['localhost'],
+    'database': {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": "awa",
+        },
+    },
+    'apps': [],
+    'urls': [],
+    'secret_key': 'asdf',
+    'debug': False,
+    'social': {
+        'redirect_url': 'awa:redirect',
+        'github': {
+            'enabled': False,
+            'key': None,
+            'secret': None,    
+        },
+        'google': {
+            'enabled': False,
+            'key': None,
+            'secret': None,
+        },
+        'facebook': {
+            'enabled': False,
+            'app': None,
+            'secret': None
+        },
+    },
+}
