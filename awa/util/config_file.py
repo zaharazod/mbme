375eedd8ffa06861195c787c3b37bc93e5f57db0
Matt Barry <matt@hazelmollusk.org>
2024-01-25 14:54:28 -0500

    OMG it fn works DO NOT TOUCH ANYTHING

--- awa/util/config_file.py
+++ awa/util/config_file.py
@@ -35,19 +35,54 @@ class ConfigFile(MissingAttrDict):
 
 
 class AwaConfig(ConfigFile):
-    def __init__(self, data=None, *a, base_path=None, process=True, **kw):
+
+    # never tell an engineer something is over-engineered
+    #
+    # i'll engineer even harder
+    _templates = ('storage',)
+    _constant_templates = (
+        lambda m, k: f'{m.upper()}_{k.upper()}',
+    )
+
+    def __init__(self, data=None, *a, base_path=None, **kw):
         self._base_path = Path(base_path) if base_path \
             else None  # Path(__file__).resolve().parent.parent.parent
         super().__init__(data, *a, **kw)
-        # print(self, base_path, self._base_path, a, kw, sep=' ||| ')
         if self._base_path:
             self.load(self._base_path / 'awa' / 'defaults.json')
             self.load(self._base_path / 'config' / 'config.json')
-            if process:
-                self.process()
+            self.initialize()
 
-    def process(self):
+    def initialize(self):
+        self.init_templates()
+        self.init_env()
+
+    def init_env(self):
+        # set any environment variables from config
         if self.env and isinstance(self.env, dict):
             os.environ.update(self.env.to_dict()  # unclear if needed
-                              if callable(self.env.to_dict)
-                              else self.env)
+                            if callable(self.env.to_dict)
+                            else self.env)
+
+    def init_templates(self):
+        # fill in some reasonable defaults (from defaults.json)
+        # fills in self.constants with key/vals to set
+        #   (eg. STATIC_URL, etc.)
+        self.setdefault('constants', dict())
+        for key in self._templates:
+            items = self[key].items()
+            template_items = list(filter(lambda zv: isinstance(zv[1], dict), items))
+            defaults = list(filter(lambda zv: isinstance(zv[1], str), items))
+            for k, _ in template_items:
+                for dk, dv in defaults:
+                    self[key][k].setdefault(dk, dv)
+                    if r'%s' in self[key][k][dk]:
+                        self[key][k][dk] = self[key][k][dk] % k
+                    if dk == 'root' and not self[key][k][dk].startswith('/'):
+                        self[key][k][dk] = str(self._base_path / self[key][k][dk])
+                    for cfunc in self._constant_templates:
+                        ck = cfunc(k, dk)
+                        if ck:
+                            self.constants.setdefault(ck, self[key][k][dk])
+        
+        
