87534d25ee95ac9784fb0862d372389b12409afb
Matt Barry <matt@hazelmollusk.org>
2024-05-09 10:25:13 -0400

    .

--- apps/ara/urls.py
+++ apps/ara/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import view_context
 
 urlpatterns = [
-    path("<path:path>", view_context),
-    path("", view_context),
+    path("<path:path>", view_context, name="context"),
+    path("", view_context, name="index"),
 ]
