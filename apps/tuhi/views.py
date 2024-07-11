7f1f4ef387460785755c618aaeec88601cd76e7a
Matt Barry <matt@hazelmollusk.org>
2024-07-11 07:56:13 -0400

    fix project link icons

--- apps/tuhi/views.py
+++ apps/tuhi/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, get_object_or_404
-from logging import warning
+from django.shortcuts import render  # , get_object_or_404
+# from logging import warning
 from apps.ara.decorators import context_view
+from awa.models import ProjectLink
 
 
 @context_view("tuhi.Page")
 def view_page(request, target, *args, **kwargs):
-    from .models import Page
-
+    # from .models import Page
+    icons = ProjectLink.objects.filter(icon__isnull=False)
     # warning(f"view_page({path})")
     # page = path if isinstance(path, Page) else get_object_or_404(Page, path=path)
     # warning(f"view_page({path}) -> [{page}]")
-    return render(request, "pages/page.html", {"page": target})
+    return render(request, "pages/page.html", {"page": target, 'icons': icons})
