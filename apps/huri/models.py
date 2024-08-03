76c7f8884524288f777d23a08ea084f531f33bb9
Matt Barry <matt@hazelmollusk.org>
2024-08-03 09:31:29 -0400

    huri.models

--- apps/huri/models.py
+++ apps/huri/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from colorfield.fields import ColorField
 
-# Create your models here.
+from awa.models import IconMixin
+
+
+class Icon(IconMixin):
+    pass
+
+
+class Theme(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    label = ColorField(default='#000000')
+    label_bg = ColorField(default='#FF0000')
+
+    add = models.ForeignKey(
+        Icon, on_delete=models.CASCADE, related_name='+')
+    change = models.ForeignKey(
+        Icon, on_delete=models.CASCADE, related_name='+')
+    delete = models.ForeignKey(
+        Icon, on_delete=models.CASCADE, related_name='+')
