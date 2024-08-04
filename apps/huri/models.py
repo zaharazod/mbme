a4a68ff1cdc02429e39bddf9b399fa1674bfb350
Matt Barry <matt@hazelmollusk.org>
2024-08-04 08:36:05 -0400

    newicons

--- apps/huri/models.py
+++ apps/huri/models.py
@@ -9,6 +9,12 @@ class Icon(IconMixin):
         return self.icon.name
 
 
+ICON_TYPES = (
+    'logo',
+)
+ICON_TYPE_CHOICES = list(zip(range(0, len(ICON_TYPES)), ICON_TYPES))
+
+
 class Theme(models.Model):
     name = models.CharField(max_length=32, unique=True)
     label = ColorField(default='#000000')
@@ -17,12 +23,20 @@ class Theme(models.Model):
     def __str__(self):
         return self.name
 
-    add = models.ForeignKey(
-        Icon, on_delete=models.CASCADE,
-        related_name='+', blank=True, null=True)
-    change = models.ForeignKey(
-        Icon, on_delete=models.CASCADE,
-        related_name='+', blank=True, null=True)
-    delete = models.ForeignKey(
-        Icon, on_delete=models.CASCADE,
-        related_name='+', blank=True, null=True)
+
+class ThemeIcon(IconMixin):
+    icon_type = models.PositiveSmallIntegerField(
+        default=0,
+        choices=ICON_TYPE_CHOICES)
+    theme = models.ForeignKey(
+        Theme,
+        related_name='icons',
+        on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['theme', 'icon_type'],
+                name='unique_icon_type_per_theme'
+            ),
+        ]
