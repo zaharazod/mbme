a41cb8cc72a8d9979ee59855f67c48a12012a992
Matt Barry <matt@hazelmollusk.org>
2025-01-21 10:08:17 -0500

    .

--- awa/admin.py
+++ awa/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import ProjectLink
+from .models import Link
 
 
-admin.site.register(ProjectLink)
+admin.site.register(Link)
